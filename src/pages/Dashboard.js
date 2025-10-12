8cc176cc8edc7c2489d837f298c5bfba18cd1d50
prajnesh23 <105186940+PRAJNESH-SADE@users.noreply.github.com>
2025-10-12 21:07:53 +0530

    fixed sidebar height in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -203,7 +203,7 @@ const Dashboard = () => {
     >
       {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 z-40 w-64 bg-white border-r border-[var(--border-color)] shadow-lg h-screen flex flex-col transform transition-transform duration-300 ease-in-out lg:static lg:translate-x-0 ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed inset-y-0 left-0 z-40 w-64 bg-white border-r border-[var(--border-color)] shadow-lg min-h-screen flex flex-col transform transition-transform duration-300 ease-in-out lg:static lg:translate-x-0 ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
           }`}
       >
         <Sidebar />
